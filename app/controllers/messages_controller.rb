6d5d1d4b9dee6593755a7f992de71694013f1b3d
Ian Andersen <ianderse@mac.com>
2015-03-28 08:21:55 -0600

    index/show pages for messages; basic functionality complete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,32 @@
 class MessagesController < ActionController::Base
   def index
+    @messages = Message.where('to_user = ?', current_user.email)
+  end
 
+  def show
+    @message = Message.find(params[:id])
   end
 
   def new
     @users = User.where('email != ?', current_user.email).pluck(:email)
     @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.from = current_user.email
+    if @message.save
+      flash[:notice] = "Message Sent"
+      redirect_to root_path
+    else
+      flash[:errors] = @message.errors.full_messages
+      render 'new'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:to_user, :subject, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328140903) do
+ActiveRecord::Schema.define(version: 20150328141956) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "body"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150328140903) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "to_user"
+    t.string   "from"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id"
